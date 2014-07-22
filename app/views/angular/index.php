a2ea0a1c68f25a0b6a907d6d8710866421d115c6
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2014-07-21 22:38:32 -0700

    add columns and missing angular map

--- app/views/angular/index.php
+++ app/views/angular/index.php
@@ -1,5 +1,23 @@
 <div data-ng-app="rightNowApp">
     <div data-ng-controller="RightNowController">
-        <h1>Right Now</h1>
+        <div class="col-md-12">
+            <h1>Right Now</h1>
+            <h3>Courthouse Area</h3>
+            <p class="text-muted timestamp">{{ courthouseTimestamp }}</p>
+        </div>
+        <!-- split into 4 columns -->
+        <div class="col-md-3">
+            column 1
+        </div>
+        <div class="col-md-3">
+            column 2
+        </div>
+        <div class="col-md-3">
+            column 3
+        </div>
+        <div class="col-md-3">
+            column 4
+        </div>
+
     </div>
 </div>
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,6 +2,11 @@
 
 var rightNowApp = angular.module('rightNowApp', []);
 
-rightNowApp.controller('RightNowController', function($scope) {
+rightNowApp.controller('RightNowController', function($scope, $http) {
+    $http.get('http://192.168.1.34/weather-station/data.php')
+        .success(function($data) {
 
+            $scope.courthouseTimestamp = $data.timestamp;
+            console.log($data);
+        });
 });
