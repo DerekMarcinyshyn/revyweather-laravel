2adff823bf675f6c00480c0afc2c5b836fff43f8
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2014-07-21 20:35:58 -0700

    add layout to default error page

--- app/views/errors/default.blade.php
+++ app/views/errors/default.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>Default error</h1>
-    <p>Oopsie...looks like something went wrong.</p>
-    <p>We logged it and will look into it.</p>
-    <p>Thanks</p>
+<div class="row">
+    <div class="container">
+        <h1>Default error</h1>
+        <p>Oopsie...looks like something went wrong.</p>
+        <p>We logged it and will look into it.</p>
+        <p>Thanks</p>
+    </div>
+</div>
 @stop
