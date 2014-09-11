ee2955306cade2ae2087a6477bd2f8d7af2cfa99
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2014-09-10 23:51:55 -0700

    add security check from SensioLabs

--- app/start/artisan.php
+++ app/start/artisan.php
@@ -4,3 +4,4 @@ Artisan::resolve('ForecastioRevelstokeCommand');
 Artisan::resolve('GetCourthouseCommand');
 Artisan::resolve('EnvironmentCanadaRevelstokeCommand');
 Artisan::resolve('CourthouseImageCommand');
+Artisan::add(new SensioLabs\Security\Command\SecurityCheckerCommand(new SensioLabs\Security\SecurityChecker));
