339c7868beba315521ebb193c5858e97c663577e
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2014-09-28 23:09:01 -0700

    add database importer from old site

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,34 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('RevyweatherCurrentTableSeeder');
 	}
 
+}
+
+class RevyweatherCurrentTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('revyweather_current')->truncate();
+
+        $dataFileHandler = fopen(app_path('assets/data/Weather.csv'), "r");
+
+        while ($data = fgetcsv($dataFileHandler))
+        {
+            Current::create([
+                'id'                => trim($data[0], '"'),
+                'temp'              => trim($data[1], '"'),
+                'humidity'          => trim($data[2], '"'),
+                'relative_humidity' => trim($data[3], '"'),
+                'bmp_temperature'   => trim($data[4], '"'),
+                'barometer'         => trim($data[5], '"'),
+                'direction'         => trim($data[6], '"'),
+                'speed'             => trim($data[7], '"'),
+                'created_at'        => trim($data[8], '"'),
+                'updated_at'        => trim($data[8], '"')
+            ]);
+        }
+        fclose($dataFileHandler);
+    }
 }
