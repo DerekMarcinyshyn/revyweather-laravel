ac9d0ac003445467be9d516757cef51d2cd0928b
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2015-09-11 23:25:31 -0700

    change view of embedded Windyty to rain and clouds

--- app/views/content/map.blade.php
+++ app/views/content/map.blade.php
@@ -9,6 +9,6 @@ class="map"
 @stop
 
 @section('content')
-    <iframe src="https://embed.windyty.com/?surface,wind,now,52.988,-115.312,5,,menu,message," width="100%" height="95%" frameborder="0"></iframe>
+    <iframe src="https://embed.windyty.com/?700h,clouds,now,52.988,-115.312,5,,menu,message," width="100%" height="95%" frameborder="0"></iframe>
 @stop
 
