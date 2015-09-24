949086dccc2929fe55e4c917fc5ceb47eda80293
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2015-09-23 23:56:02 -0700

    add preloader to map

--- app/views/content/map.blade.php
+++ app/views/content/map.blade.php
@@ -9,6 +9,8 @@ class="map"
 @stop
 
 @section('content')
-    <iframe src="https://embed.windyty.com/?700h,clouds,now,52.988,-115.312,5,,menu,message," width="100%" height="95%" frameborder="0"></iframe>
+    <div class="map-preloader">
+        <iframe src="https://embed.windyty.com/?700h,clouds,now,52.988,-115.312,5,,menu,message," width="100%" height="95%" frameborder="0"></iframe>
+    </div>
 @stop
 
