90eecf4e4ab3adfeabfb8aae9b4cda59407ef0c8
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2016-03-20 16:05:35 -0700

    configure exlixir

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,29 @@
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix
+        // css
+        .copy('bower_components/angular-material/angular-material.min.css', 'resources/assets/css/angular-material.min.css')
+        .styles([
+            'angular-material.min.css',
+            'app.css'
+        ])
+
+        // javascript
+        .copy('bower_components/angular/angular.min.js', 'resources/assets/js/angular.min.js')
+        .copy('bower_components/angular-aria/angular-aria.min.js', 'resources/assets/js/angular-aria.min.js')
+        .copy('bower_components/angular-animate/angular-animate.min.js', 'resources/assets/js/angular-animate.min.js')
+        .copy('bower_components/angular-material/angular-material.min.js', 'resources/assets/js/angular-material.min.js')
+        .scripts([
+            'angular.min.js',
+            'angular-aria.min.js',
+            'angular-animate.min.js',
+            'angular-material.min.js'
+        ])
+
+        .version([
+            'css/all.css',
+            'js/all.js'
+        ]);
 });
+    
