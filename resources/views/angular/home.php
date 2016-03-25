b64cad444bcddada866c333ce5e795f7b3269375
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2016-03-24 20:12:57 -0700

    add conditions homepage css

--- resources/views/angular/home.php
+++ resources/views/angular/home.php
@@ -1,38 +1,46 @@
 <div ng-controller="HomeController" ng-cloak>
-    <section >
-        <h1>Courthouse Revelstoke</h1>
-    </section>
+    <md-content layout-xs="column" layout="row">
 
-    <section>
-        <h1>Downtown Revelstoke</h1>
-    </section>
+        <md-card flex="66" flex-xs="100" flex-sm="90">
+            <md-card-title class="md-headline">Courthouse Revelstoke</md-card-title>
+        </md-card>
 
-    <section>
-        <h1>Revelstoke Airport</h1>
-        <p>{{ airport.currentConditions.dateTime[1].textSummary }}</p>
+        <md-card flex="66" flex-xs="100" flex-sm="90">
+            <md-card-title class="md-headline">Downtown Revelstoke</md-card-title>
+        </md-card>
 
-        <article class="conditions">
-            <div flex>
-                <img ng-src="img/ec/icons-large/{{ airport.currentConditions.iconCode }}.png" alt="" width="80" height="80" />
-                <div class="current-temperature">
-                    {{ airport.currentConditions.temperature | number:1 }}&deg;C
-                </div>
-            </div>
-            <div flex>
-                <p><span class="text-muted">Condition:</span> {{ airport.currentConditions.condition }}</p>
-                <p><span class="text-muted">Pressure:</span> {{ airport.currentConditions.pressure | number:1 }}kPa</p>
-                <p><span class="text-muted">Humidity:</span> {{ airport.currentConditions.relativeHumidity | number:0 }}%</p>
-                <p><span class="text-muted">Wind:</span> {{ airport.currentConditions.wind.direction | ecDirection }} &nbsp; {{ airport.currentConditions.wind.speed | number:1 }} km/h</p>
-            </div>
-            <div flex>
-                Wind speed gauge
-            </div>
-            <div flex>
-                Compass gauge
-            </div>
-        </article>
-        <article class="forecast">
-            forecast
-        </article>
-    </section>
+        <md-card flex="66" flex-xs="100" flex-sm="90">
+            <md-card-title class="md-headline">Revelstoke Airport</md-card-title>
+            <md-card-content>
+                <p class="text-summary">{{ airport.currentConditions.dateTime[1].textSummary }}</p>
+                <article class="conditions" layout="column">
+                    <div layout="row">
+                        <div flex="50">
+                            <img src="" ng-src="img/ec/icons-large/{{ airport.currentConditions.iconCode }}.png" alt="" width="80" height="80" />
+                        </div>
+                        <div flex="50">
+                            <span class="text-temperature">{{ airport.currentConditions.temperature | number:1 }}&deg;C</span>
+                        </div>
+                    </div>
+                    <div layout="column" class="text-conditions">
+                        <p><span class="text-summary">Condition:</span> {{ airport.currentConditions.condition }}</p>
+                        <p><span class="text-summary">Pressure:</span> {{ airport.currentConditions.pressure | number:1 }}kPa</p>
+                        <p><span class="text-summary">Humidity:</span> {{ airport.currentConditions.relativeHumidity | number:0 }}%</p>
+                        <p><span class="text-summary">Wind:</span> {{ airport.currentConditions.wind.direction | ecDirection }} &nbsp; {{ airport.currentConditions.wind.speed | number:1 }} km/h</p>
+                    </div>
+                    <div layout="row">
+                        <div flex="50">
+                            <p>Wind speed gauge</p>
+                        </div>
+                        <div flex="50">
+                            <p>Compass gauge</p>
+                        </div>
+                    </div>
+                </article>
+                <article class="forecast">
+                    <h2>forecast</h2>
+                </article>
+            </md-card-content>
+        </md-card>
+    </md-content>
 </div>
