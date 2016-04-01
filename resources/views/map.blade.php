88efd86895c86bd08cde65f29cf6f4f9d938f513
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2016-03-31 23:21:23 -0700

    add windyty weather map

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('content')
-<section>
-    <h1>Map</h1>
-</section>
+    <div class="map-preloader">
+        <iframe src="https://embed.windyty.com/?700h,clouds,now,52.988,-115.312,5,,menu,message," width="100%" height="95%" frameborder="0"></iframe>
+    </div>
 @endsection
