1e3e70be0f13320dbbed59018ed49ecaced95c23
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2016-04-09 00:31:04 -0700

    fix webcams layout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,8 +1,14 @@
 @extends('layout')
 
 @section('content')
-<md-content flex layout-padding>
-    <h1>About</h1>
-    <p>RevyWeather.com is a side project for playing with hardware and software.</p>
+<md-content layout="column" layout-fill>
+    <div layout="row" layout-align="center">
+        <md-card flex="100" flex-gt-md="66">
+            <md-card-title><h2>About</h2></md-card-title>
+            <md-card-content>
+                <p>RevyWeather.com is a side project for playing with hardware and software.</p>
+            </md-card-content>
+        </md-card>
+    </div>
 </md-content>
 @endsection
--- resources/views/angular/webcams.php
+++ resources/views/angular/webcams.php
@@ -1,22 +1,21 @@
 <div ng-controller="WebcamsController" ng-cloak>
-    <md-content class="webcams-bg" layout="column" layout-padding>
-        <md-grid-list md-cols-gt-md="3" md-cols="1"
-                      md-row-height-gt-md="1:1" md-row-height="2:2"
-                      md-gutter-gt-md="16px" md-gutter="20px">
-            <md-grid-tile ng-repeat="webcam in webcams">
-                <div class="webcams md-whiteframe-10dp">
-                    <h2 class="md-headline">
-                        {{ webcam.title }}
-                        <md-button style="float:right"
-                                   class="md-icon-button"
-                                   aria-label="Expand"
-                                   ng-click="expand($event, webcam.url)">
-                            <i class="fa fa-expand"></i>
-                        </md-button>
-                    </h2>
-                    <img src="" ng-src="{{ webcam.url }}" width="100%" height="auto" />
-                </div>
-            </md-grid-tile>
-        </md-grid-list>
-    </md-content>
+    <md-grid-list md-cols-gt-md="3" md-cols="1"
+                  md-row-height-gt-md="1:1" md-row-height="2:2"
+                  md-gutter-gt-md="6px" md-gutter="10px" class="webcams-bg">
+        <md-grid-tile ng-repeat="webcam in webcams">
+            <div class="webcams md-whiteframe-10dp">
+                <h2 class="md-headline">
+                    {{ webcam.title }}
+                    <md-button style="float:right"
+                               class="md-icon-button"
+                               aria-label="Expand"
+                               ng-click="expand($event, webcam.url)">
+                        <i class="fa fa-expand"></i>
+                    </md-button>
+                </h2>
+                <img src="http://www.revelstokemountainresort.com/uploads/webcams/gnome.jpg"
+                     ng-src="{{ webcam.url }}" width="100%" height="auto" />
+            </div>
+        </md-grid-tile>
+    </md-grid-list>
 </div>
