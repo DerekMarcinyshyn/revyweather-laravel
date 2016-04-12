239d5b43dbca80410a12b425d524aa2fac8444aa
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2016-04-12 10:10:18 -0700

    update Envoy

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -2,12 +2,19 @@
 
 @macro('deploy', ['on' => 'web'])
     list
+    fetch_repo
     update_permissions
 @endmacro
 
 @task('list')
     cd /var/www/vhosts/revelstokewebhosting.net/revyweather.ca;
     ls -la
+@endtask
+
+@task('fetch_repo')
+    cd /var/www/vhosts/revelstokewebhosting.net/revyweather.ca;
+    git fetch origin master;
+    git reset --hard FETCH_HEAD;
 @endtask
 
 @task('update_permissions')
