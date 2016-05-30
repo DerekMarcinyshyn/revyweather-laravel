7b035af2d4ec69b62e503485035d101d1859fb21
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2016-05-29 21:56:22 -0700

    change Environment Canada to check every ten minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -36,7 +36,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('revyweather:environment-canada')
-            ->hourly();
+            ->everyTenMinutes();
         
         $schedule->command('revyweather:forecast-io')
             ->everyTenMinutes();
