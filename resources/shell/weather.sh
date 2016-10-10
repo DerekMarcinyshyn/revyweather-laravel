de9d4c5c67a96e50fe6f04c8f8f8dbb75d858dcf
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2016-10-10 15:37:15 -0700

    change back to every 5 seconds

--- resources/shell/weather.sh
+++ resources/shell/weather.sh
@@ -3,7 +3,7 @@
 # Get the weather data from the local server every 5 seconds
 # @author   Derek Marcinyshyn
 # @date     2016-10-10
-while (sleep 10 && /usr/bin/php /var/www/vhosts/revyweather.ca/httpdocs/revyweather-laravel/artisan revyweather:local)
+while (sleep 5 && /usr/bin/php /var/www/vhosts/revyweather.ca/httpdocs/revyweather-laravel/artisan revyweather:local)
 do
     wait $!
 done
