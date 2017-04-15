75ec60fb0fc5f3fd24e5a7a4309aa03c4df634dc
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2017-04-14 23:36:34 -0700

    update windy.tv map

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <div class="map-preloader">
-        <iframe src="https://embed.windyty.com/?700h,clouds,now,52.988,-115.312,5,,menu,message," width="100%" height="95%" frameborder="0"></iframe>
+        <iframe width="100%" height="96%" src="https://embed.windytv.com/embed2.html?lat=51.000&lon=-118.183&zoom=5&level=700h&overlay=wind&menu=&message=&marker=&forecast=12&calendar=now&location=coordinates&type=map&actualGrid=&metricWind=km%2Fh&metricTemp=%C2%B0C" frameborder="0"></iframe>
     </div>
 @endsection
