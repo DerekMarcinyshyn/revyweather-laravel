88f95c96e075051b0da5bad50af669fbf5903c1c
DerekMarcinyshyn <derek@monasheemountainmultimedia.com>
2017-04-29 12:15:40 -0700

    reorder homepage wx panels

--- resources/views/angular/home.php
+++ resources/views/angular/home.php
@@ -59,76 +59,6 @@
         </div>
 
         <div layout="row" layout-align="center">
-            <md-progress-circular md-mode="indeterminate" ng-hide="showDowntown" class="md-primary" md-diameter="40"></md-progress-circular>
-            <md-card ng-show="showDowntown" flex="100" flex-gt-md="66">
-                <md-card-title class="md-headline">Downtown Revelstoke</md-card-title>
-                <md-card-content>
-                    <p class="text-summary">{{ darkSky.currently.time * 1000 | date:'medium' }}</p>
-                    <div class="conditions" layout-padding layout="column" layout-gt-xs="row">
-                        <div flex-gt-xs="25" layout="row" layout-gt-xs="column">
-                            <div flex="50" layout="column">
-                                <skycon icon="currentWeather.forecast.icon" color="black" size="currentWeather.forecast.iconSize"></skycon>
-                            </div>
-                            <div flex="50" layout="column" class="container-temperature">
-                                <span class="text-temperature">{{ darkSky.currently.temperature | number:1 }}&deg;C</span>
-                            </div>
-                        </div>
-                        <div flex-gt-xs="25" layout="column" class="text-conditions">
-                            <p><span class="text-summary">Condition:</span> {{ darkSky.currently.summary }}</p>
-                            <p><span class="text-summary">Pressure:</span> {{ darkSky.currently.pressure / 10 | number:1 }}kPa</p>
-                            <p><span class="text-summary">Humidity:</span> {{ darkSky.currently.humidity * 100 | number:0 }}%</p>
-                            <p><span class="text-summary">Wind:</span> {{ darkSky.currently.windBearing | windDirection }} &nbsp; {{ darkSky.currently.windSpeed | number:1 }} km/h</p>
-                        </div>
-                        <div flex-gt-xs="50" layout="row">
-                            <div flex="50">
-                                <div id="gauge-downtown" style="width:150px;height:110px;"></div>
-                            </div>
-                            <div flex="50">
-                                <div class="compass">
-                                    <div class="compass-inner">
-                                        <div class="north">N</div>
-                                        <div class="east">E</div>
-                                        <div class="west">W</div>
-                                        <div class="south">S</div>
-                                        <div data-ng-style="arrowDowntown" class="main-arrow">
-                                            <div class="arrow-up"></div>
-                                            <div class="arrow-down"></div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <h2 layout="row" class="forecast" layout-padding>Forecast</h2>
-                    <p layout="row" layout-padding>{{ darkSky.daily.summary }}</p>
-                    <div class="forecast" layout="column" layout-gt-xs="row">
-                        <md-whiteframe ng-repeat="darkSky in darkSky.daily.data | limitTo:5"
-                                       class="md-whiteframe-4dp layout-padding layout-margin"
-                                       flex-gt-xs="20"
-                                       layout-gt-xs="column">
-                            <h3>{{ darkSky.time * 1000 | date:'EEEE' }}</h3>
-                            <div layout-gt-xs="column" layout="row">
-                                <div flex="50">
-                                    <div class="climacon {{ darkSky.icon | climacons }}"></div>
-                                </div>
-                                <div flex="50">
-                                    <p>
-                                        <span class="label-high">HIGH</span> &nbsp; <span class="text-temperature">{{ darkSky.temperatureMax | number:0 }}&deg;C</span>
-                                    </p>
-                                    <p>
-                                        <span class="label-low">LOW</span> &nbsp; <span class="text-temperature">{{ darkSky.temperatureMin | number:0 }}&deg;C</span>
-                                    </p>
-                                </div>
-                            </div>
-                            <p>{{ darkSky.summary }}</p>
-                        </md-whiteframe>
-                    </div>
-                    <p class="powered-by">Powered by <a href="https://darksky.net" target="_blank">Dark Sky</a> updated every 10 minutes.</p>
-                </md-card-content>
-            </md-card>
-        </div>
-
-        <div layout="row" layout-align="center">
             <md-progress-circular md-mode="indeterminate" ng-hide="showAirport" class="md-primary" md-diameter="40"></md-progress-circular>
             <md-card ng-show="showAirport" flex="100" flex-gt-md="66">
                 <md-card-title class="md-headline">Revelstoke Airport</md-card-title>
@@ -199,6 +129,76 @@
                         </md-whiteframe>
                     </div>
                     <p class="powered-by">Powered by <a href="http://weather.gc.ca/city/pages/bc-65_metric_e.html" target="_blank">Environment Canada</a> updated every hour.</p>
+                </md-card-content>
+            </md-card>
+        </div>
+
+        <div layout="row" layout-align="center">
+            <md-progress-circular md-mode="indeterminate" ng-hide="showDowntown" class="md-primary" md-diameter="40"></md-progress-circular>
+            <md-card ng-show="showDowntown" flex="100" flex-gt-md="66">
+                <md-card-title class="md-headline">Downtown Revelstoke</md-card-title>
+                <md-card-content>
+                    <p class="text-summary">{{ darkSky.currently.time * 1000 | date:'medium' }}</p>
+                    <div class="conditions" layout-padding layout="column" layout-gt-xs="row">
+                        <div flex-gt-xs="25" layout="row" layout-gt-xs="column">
+                            <div flex="50" layout="column">
+                                <skycon icon="currentWeather.forecast.icon" color="black" size="currentWeather.forecast.iconSize"></skycon>
+                            </div>
+                            <div flex="50" layout="column" class="container-temperature">
+                                <span class="text-temperature">{{ darkSky.currently.temperature | number:1 }}&deg;C</span>
+                            </div>
+                        </div>
+                        <div flex-gt-xs="25" layout="column" class="text-conditions">
+                            <p><span class="text-summary">Condition:</span> {{ darkSky.currently.summary }}</p>
+                            <p><span class="text-summary">Pressure:</span> {{ darkSky.currently.pressure / 10 | number:1 }}kPa</p>
+                            <p><span class="text-summary">Humidity:</span> {{ darkSky.currently.humidity * 100 | number:0 }}%</p>
+                            <p><span class="text-summary">Wind:</span> {{ darkSky.currently.windBearing | windDirection }} &nbsp; {{ darkSky.currently.windSpeed | number:1 }} km/h</p>
+                        </div>
+                        <div flex-gt-xs="50" layout="row">
+                            <div flex="50">
+                                <div id="gauge-downtown" style="width:150px;height:110px;"></div>
+                            </div>
+                            <div flex="50">
+                                <div class="compass">
+                                    <div class="compass-inner">
+                                        <div class="north">N</div>
+                                        <div class="east">E</div>
+                                        <div class="west">W</div>
+                                        <div class="south">S</div>
+                                        <div data-ng-style="arrowDowntown" class="main-arrow">
+                                            <div class="arrow-up"></div>
+                                            <div class="arrow-down"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <h2 layout="row" class="forecast" layout-padding>Forecast</h2>
+                    <p layout="row" layout-padding>{{ darkSky.daily.summary }}</p>
+                    <div class="forecast" layout="column" layout-gt-xs="row">
+                        <md-whiteframe ng-repeat="darkSky in darkSky.daily.data | limitTo:5"
+                                       class="md-whiteframe-4dp layout-padding layout-margin"
+                                       flex-gt-xs="20"
+                                       layout-gt-xs="column">
+                            <h3>{{ darkSky.time * 1000 | date:'EEEE' }}</h3>
+                            <div layout-gt-xs="column" layout="row">
+                                <div flex="50">
+                                    <div class="climacon {{ darkSky.icon | climacons }}"></div>
+                                </div>
+                                <div flex="50">
+                                    <p>
+                                        <span class="label-high">HIGH</span> &nbsp; <span class="text-temperature">{{ darkSky.temperatureMax | number:0 }}&deg;C</span>
+                                    </p>
+                                    <p>
+                                        <span class="label-low">LOW</span> &nbsp; <span class="text-temperature">{{ darkSky.temperatureMin | number:0 }}&deg;C</span>
+                                    </p>
+                                </div>
+                            </div>
+                            <p>{{ darkSky.summary }}</p>
+                        </md-whiteframe>
+                    </div>
+                    <p class="powered-by">Powered by <a href="https://darksky.net" target="_blank">Dark Sky</a> updated every 10 minutes.</p>
                 </md-card-content>
             </md-card>
         </div>
